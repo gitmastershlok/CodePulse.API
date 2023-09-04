0686748ac2410aae86d6cbe466fd5d5534d99b95
Shlok B. Gupta <shlok.gupta_cs19@gla.ac.in>
2023-09-04 17:59:07 +0530

    Add project files.

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Data;
+using CodePulse.API.Repositories.Interface;
+
+namespace CodePulse.API.Controllers
+{
+    // https://locaalhost:xxxx/api/categories
+    [Route("api/[controller]")]
+	[ApiController]
+	public class CategoriesController : ControllerBase
+	{
+		private readonly ICategoryRepository categoryRepository;
+
+		public CategoriesController(ICategoryRepository categoryRepository)
+        {
+			this.categoryRepository = categoryRepository;
+		}
+
+        [HttpPost]
+		public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+		{
+			//Map DTO model to Domain model
+			Category category = new Category()
+			{
+				Name = request.Name,
+				UrlHandle = request.UrlHandle
+			};
+
+			await categoryRepository.CreateAsync(category);
+
+			//Map Domain model to DTO model
+			CategoryDto response = new CategoryDto()
+			{
+				Id = category.Id,
+				Name = category.Name,
+				UrlHandle = category.UrlHandle
+			};
+			return Ok(response);
+		}
+	}
+}
